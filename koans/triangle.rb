212c94cd68053cf1e944825f5439b0f0e6b33fc9
Fuffi <ufinii@gmail.com>
2012-02-02 14:45:42 +0100

    renamed

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,10 +14,10 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  sides = [a, b, c].uniq
-  return :equilateral if sides.length == 1
-  return :isosceles if sides.length == 2
-  return :scalene if sides.length == 3
+  unique_lengths = [a, b, c].uniq
+  return :equilateral if unique_lengths.length == 1
+  return :isosceles if unique_lengths.length == 2
+  return :scalene if unique_lengths.length == 3
 end
 
 # Error class used in part 2.  No need to change this code.
