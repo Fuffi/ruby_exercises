8e90282c5b74dae02d2b8b6b57a5384934da069e
Federico Rebora <federicorebora@federico-reboras-macbook.local>
2012-02-06 00:03:13 +0100

    refactored problem 6

--- project_euler/6.rb
+++ project_euler/6.rb
@@ -1,10 +1,9 @@
 def sum_of_squares_up_to(number)
-  (1..number).inject(0) { |sum, x| sum + x * x }
+  (1..number).inject(0) { |sum, x| sum + x ** 2 }
 end
 
 def square_of_the_sums_up_to(number)
-  sum = (1..number).inject(:+)
-  sum * sum
+  sum = (1..number).inject(:+) ** 2
 end
 
 print square_of_the_sums_up_to(100) - sum_of_squares_up_to(100)
